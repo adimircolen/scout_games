68ef87dd737d37b0b679f4c31fc5081eb75934e9
Adimir Colen <adimircolen@gmail.com>
2012-10-25 00:19:27 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoutGames::Application.config.secret_token = 'a8cadb6a7da8fa31c1a3a1413f7a37d7011e409072f4a780224b21a10a2d70ad0a39baaf6a83759bee7095c85e8d45f41a032b2f49f3f80a880641f07e90fbf7'
